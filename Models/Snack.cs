8a8e87827aa276fc4fad0d195bf3c63f59078a14
mfratassi <mfratassi@gmail.com>
2019-10-05 23:41:53 -0300

    Start: Snack and SnackCategory Model Entities

---
+++ Models/Snack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LanchesWeb.Models
+{
+    public class Snack
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ShorDescription { get; set; }
+        public string LongDescription { get; set; }
+        public sbyte MainImage { get; set; }
+        public sbyte MainThumbnail { get; set; }
+        public int SnackCategoryId { get; set; }
+        public virtual SnackCategory SnackCategory { get; set; }
+        public bool IsStarred { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Snack() { }
+
+    }
+}
---
+++ Models/SnackCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LanchesWeb.Models
+{
+    public class SnackCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Snack> Snacks{ get; set; }
+
+    }
+}
