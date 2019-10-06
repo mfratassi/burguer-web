dc768b77c85ad529ae43bf12d5b60468ae4297c2
mfratassi <mfratassi@gmail.com>
2019-10-06 00:01:42 -0300

    Database Configure Startup and Connection Strings

---
+++ Data/LanchesWebContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesWeb.Models
+{
+    public class LanchesWebContext : DbContext
+    {
+        public LanchesWebContext(DbContextOptions<LanchesWebContext> options) : base(options) { }
+
+        public DbSet<Snack> Snacks { get; set; }
+
+        public DbSet<SnackCategory> SnackCategories { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using LanchesWeb.Models;
 
 namespace LanchesWeb
 {
@@ -24,6 +26,7 @@ namespace LanchesWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<LanchesWebContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
